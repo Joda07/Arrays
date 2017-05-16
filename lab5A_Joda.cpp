d39fb287d937698df94a41ff23198c491c6dd573
Joda07 <ijoda@bulldogs.aamu.edu>
2017-05-16 10:15:37 -0500

    Add files via upload
    1 dimensional Array for storing values in C++

---
+++ lab5A_Joda.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+	double value[10];
+	int number;
+	for(number =0; number < 10; number++)
+	{
+		cout << "Please enter a Number: "; 
+		cin >> value[number];
+	
+	}
+
+
+	system("pause");
+	return 0;
+}
